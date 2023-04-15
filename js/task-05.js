3379490c2cd94a69d8a16955247212fb7c0bbc93
Kateryna1706 <silckatya@gmail.com>
2023-04-15 15:43:12 +0200

    added trim() method to task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,10 @@
 let nameInput = document.querySelector("#name-input");
 let nameOutput = document.querySelector("#name-output");
-console.dir(nameInput.part);
 
 nameInput.addEventListener("input", (event) => {
-  if (
-    event.currentTarget.value === "" ||
-    event.currentTarget.value.includes(" ")
-  ) {
+  if (event.currentTarget.value.trim() === "") {
     nameOutput.textContent = "Anonymous";
   } else {
-    nameOutput.textContent = event.currentTarget.value;
+    nameOutput.textContent = event.currentTarget.value.trim();
   }
 });
